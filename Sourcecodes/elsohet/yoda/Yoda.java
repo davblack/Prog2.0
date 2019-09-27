c0b77fa3e102eb9b9f0cea6cd51439b4e4fca4a4
davblack <davidfekete51@gmail.com>
2019-09-27 15:28:25 +0200

    finalest927

---
+++ Sourcecodes/elsohet/yoda/Yoda.java
@@ -0,0 +1,18 @@
+
+public class Yoda {
+    public static void main(String[]args){
+
+        String a = null;
+        String b = "asd";
+        if (a.equals(b)){
+            System.out.println("OK");
+        }
+
+        /*
+        Boolean c = true;
+        if (null = c) {
+            System.out.println("OK");
+        }
+        */
+    }
+}
